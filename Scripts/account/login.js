1024c8dc2a33b4332b55b4a3eb421f2bf221bedf
hexuefeng <hexufeng1@wanda.com.cn>
2017-10-20 11:20:27 +0800

    init

---
+++ Scripts/account/login.js
@@ -0,0 +1,34 @@
+﻿$(document).ready(function () {
+    $("#btnLogin").click(function () {
+        if ($("#AccountName").val() == '') {
+            layer.msg("用户名不能为空！");
+            return;
+        }
+        if ($("#Password").val() == '') {
+            layer.msg("密码不能为空！");
+            return;
+        }
+        $.ajax({
+            url: "/Account/Login",
+            data: { AccountName: $("#AccountName").val(), Password: $("#Password").val() },
+            type: "post",
+            dataType: "json",
+            success: function (data) {
+                if (data.IsSuccess) {
+                    layer.msg('登陆成功',
+                        {
+                            time: 2000,
+                            end: function () {
+                                window.location.href = "/home/index";
+                            }
+                        });
+                } else {
+                    layer.msg(data.Message);
+                }
+            },
+            error: function () {
+                layer.msg('系统异常，请稍后尝试');
+            }
+        });
+    })
+});
